f66187bd37bf9d627365b3d43742663ddd380fcf
shoaib akhtar <s.khan957051@gmail.com>
2023-03-01 16:08:23 +0530

    need to update some files

---
+++ src/pages/EditNote.jsx
@@ -0,0 +1,31 @@
+import {Link, useParams} from "react-router-dom";
+import {IoIosArrowBack} from "react-icons/io"
+import {RiDeleteBin6Line} from "react-icons/ri"
+import { useState } from "react";
+
+
+const EditNote = ({notes, setNotes}) => {
+  const {id} = useParams();
+  const note = notes.find((item) => item.id == id);
+  console.log(note);
+  const [title, setTitle] = useState(note.title);
+  const [details, setDetails] = useState(note.details);
+  return (
+    <section>
+
+    <header className="create-note__header">
+     <Link to="/" className="btn"> <IoIosArrowBack/> </Link>
+     <button className="btn lg primary">Save</button>
+     <button className="btn danger"><RiDeleteBin6Line/></button>
+    </header>
+
+    <form className="create-note__form">
+      <input type="text" placeholder="Title" autoFocus />
+      <textarea  rows="28" placeholder="Note details..."></textarea>
+    </form>
+
+  </section>
+  )
+}
+
+export default EditNote
